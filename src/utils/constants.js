98a393beb3e47d89322e1942196ec299493efad5
DHARM <opdsbanasya@gmail.com>
2024-10-28 14:10:33 +0530

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 import building from "../assets/building.jpg";
 import mainPool from "../assets/main-pool.jpg";
-import hall from "../assets/hall.jpg";
+import hall from "../assets/event-hall.jpg";
 import buildingView from "../assets/building-view.jpg";
 import dinningArea from "../assets/dinning-area.jpg";
 import staff from "../assets/staff.jpg";
