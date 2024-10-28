6d8371077b375e6bb610caf731d514684ddc23e0
DHARM <opdsbanasya@gmail.com>
2024-10-28 18:32:30 +0530

    pricing link fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -99,9 +99,8 @@ const Navbar = () => {
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to="#" onClick={() => {
+                            <NavLink to="/pricing" onClick={() => {
                                 setShowMenu(false);
-                                handleClickScroll("pricing");
                             }} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
                                 Pricing
                             </NavLink>
